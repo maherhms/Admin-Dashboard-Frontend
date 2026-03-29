743b10e57289b269c5aeb5751d01d92cd974a81e
maherhms <maher.3000@outlook.fr>
2026-03-29 16:32:34 +0200

    Implement middleware toast feedback

--- src/providers/data.ts
+++ src/providers/data.ts
@@ -1,6 +1,26 @@
 import {createDataProvider, CreateDataProviderOptions} from "@refinedev/rest";
 import {BACKEND_BASE_URL} from "@/constants";
 import {ListResponse} from "@/types";
+import {HttpError} from "@refinedev/core";
+
+if(!BACKEND_BASE_URL) throw new Error("BACKEND_BASE_URL is not defined");
+
+const buildHttpError = async (response: Response) : Promise<HttpError> => {
+  let message = "Request failed";
+
+  try {
+    const payload = await (response.json()) as { message?: string };
+
+    if(payload?.message) message = payload.message;
+  }catch{
+    // Ignore errors
+  }
+
+  return {
+    message,
+    statusCode: response.status,
+  }
+}
 
 const options : CreateDataProviderOptions = {
   getList : {
@@ -26,12 +46,16 @@ const options : CreateDataProviderOptions = {
     },
 
     mapResponse : async(response) => {
+      if(!response.ok) throw await buildHttpError(response);
+
       const payload : ListResponse = await response.clone().json();
 
       return payload.data ?? [];
     },
 
     getTotalCount : async(response) => {
+      if(!response.ok) throw await buildHttpError(response);
+
       const payload : ListResponse = await response.clone().json();
 
       return payload.pagination?.total ?? payload.data?.length ?? 0;
